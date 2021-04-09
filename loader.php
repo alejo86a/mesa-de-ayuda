1409bdbe64dcd8952b2ec51b8de25a9a16fd1b82
Jose Alejandro Berrio Marin <alejo86a@gmail.com>
2021-04-09 16:38:24 -0500

    firts crud create files

---
+++ loader.php
@@ -0,0 +1,11 @@
+<?php
+
+    error_reporting(E_ALL);
+
+    require_once './Library/ez_sql_core.php';
+    require_once './Library/ez_sql_mysql.php';
+    require_once './src/shared/DatabaseConnection.php';
+    require_once './src/dao/AreaDao.php';
+    require_once './src/view/AreaView.php';
+    require_once './src/dto/AreaDto.php';
+
